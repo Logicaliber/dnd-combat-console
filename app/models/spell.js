22572dab9439f8019493147dabd2a235066ebdc5
Tyler Hanna <thanna3992@gmail.com>
2022-05-01 17:44:20 -0700

    Added Spell model and migration

---
+++ app/models/spell.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spell extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Spell.init({
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    level: DataTypes.INTEGER,
+    school: DataTypes.STRING,
+    castingTime: DataTypes.STRING,
+    range: DataTypes.INTEGER,
+    components: DataTypes.STRING,
+    duration: DataTypes.STRING,
+    saveType: DataTypes.STRING,
+    saveHalf: DataTypes.BOOLEAN,
+    description: DataTypes.TEXT,
+    damage: DataTypes.JSON
+  }, {
+    sequelize,
+    modelName: 'Spell',
+  });
+  return Spell;
+};
